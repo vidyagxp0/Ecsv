16812a5ac860bde7f5fc9c12625cbdf953dcb418
gauravidya <gaurav.meena@vidyagxp.com>
2024-07-09 17:17:29 +0530

    tailwind setup

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      </div>
+    </div>
+  );
+}
